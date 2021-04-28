5a2d43c2bd7d9cdd757f5ca36c23bc92f7e6b2f0
ghostman <cielbleuph@gmail.com>
2021-04-29 05:42:00 +0800

    fixed chart script in homepage.

--- index.php
+++ index.php
@@ -43,6 +43,39 @@
                 # code...
                 // include('views/proposal-tool.php');
                 include('views/home.php');
+                ?>
+                <!-- Graphs -->
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script>
+                <script>
+                  var ctx = document.getElementById("myChart");
+                  var myChart = new Chart(ctx, {
+                    type: 'line',
+                    data: {
+                      labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+                      datasets: [{
+                        data: [15339, 21345, 18483, 24003, 23489, 24092, 12034],
+                        lineTension: 0,
+                        backgroundColor: 'transparent',
+                        borderColor: '#007bff',
+                        borderWidth: 4,
+                        pointBackgroundColor: '#007bff'
+                      }]
+                    },
+                    options: {
+                      scales: {
+                        yAxes: [{
+                          ticks: {
+                            beginAtZero: false
+                          }
+                        }]
+                      },
+                      legend: {
+                        display: false,
+                      }
+                    }
+                  });
+                </script>
+                <?php
                 break;
             }
             
@@ -51,37 +84,7 @@
 
         
 
-    <!-- Graphs -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script>
-    <script>
-      var ctx = document.getElementById("myChart");
-      var myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-          labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
-          datasets: [{
-            data: [15339, 21345, 18483, 24003, 23489, 24092, 12034],
-            lineTension: 0,
-            backgroundColor: 'transparent',
-            borderColor: '#007bff',
-            borderWidth: 4,
-            pointBackgroundColor: '#007bff'
-          }]
-        },
-        options: {
-          scales: {
-            yAxes: [{
-              ticks: {
-                beginAtZero: false
-              }
-            }]
-          },
-          legend: {
-            display: false,
-          }
-        }
-      });
-    </script>
+    
 
     <?php include('views/inc/footer.php'); 
     
