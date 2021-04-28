c5509d4397000f5ce22019179fd12b1ac05ca4f3
ghostman <cielbleuph@gmail.com>
2021-04-28 22:31:17 +0800

    updated changes on structures.

--- views/inc/sidebar-nav.php
+++ views/inc/sidebar-nav.php
@@ -8,20 +8,20 @@
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="proposal-tool.php">
+                <a class="nav-link" href="?p=proposal-tool">
                   <i data-feather="tool"></i>
                   Generate Proposal
                 </a>
               </li>
               
               <li class="nav-item">
-                <a class="nav-link" href="/generate-sow.php">
+                <a class="nav-link" href="?p=generate-sow">
                   <i data-feather="file-text"></i>
                   Generate S.O.W.
                 </a>
               </li>
               <hr style="border-color: black; border-size: 1px;" />
-              <li class="nav-item">
+              <!-- <li class="nav-item">
                 <a class="nav-link" href="#">
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
                   Customers
@@ -71,7 +71,7 @@
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
                   Year-end sale
                 </a>
-              </li>
+              </li> -->
             </ul>
           </div>
         </nav>
