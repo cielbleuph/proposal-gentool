7145b329cc19e57270ad6a0b5b031041f3c5a70f
ghostman <cielbleuph@gmail.com>
2021-07-05 21:37:55 +0800

    fixed project disclaimer per services.

--- inc/process-sow.php
+++ inc/process-sow.php
@@ -262,7 +262,7 @@
     $coverPageDateTextRun = $coverPage->addTextRun( array('alignment' => 'start') );
     $coverPageDateTextRun->addText(htmlspecialchars("\t\t\t\t\t\t\t\t\t DATE:"), array('size' => 16, 'color' => 'C42543'));
     $coverPageDateTextRun->addTextBreak();
-    $coverPageDateTextRun->addText(htmlspecialchars( "\t\t\t\t\t\t\t\t\t " . $testStartDate ), array('size'=>16));
+    $coverPageDateTextRun->addText(htmlspecialchars( "\t\t\t\t\t\t\t\t\t " . $generatedDate ), array('size'=>16));
 
     $coverPage->addTextBreak(5);
 
@@ -525,17 +525,28 @@
         $projectPrereqPage->addListItem('Exported API list such as SWAGGER file, YAML or WSDL', 0);
     }
 
-    // if ( $typeOfService[0] == '' ) {
-
-    // }
-
     $projectPrereqPage->addText('Other project pre-requisites will be discussed on the Slack channel that will be opened before the test/s will be conducted.');
 
     //############################### END PROJECT PRE-REQUISITES REQUIREMENTS SCOPE PAGE ###############################   
 
     //############################### PROJECT DISCLAIMER PAGE ###############################    
+
+
+    $disclaimerTypeOne = array(
+        "API Penetration Test", 
+        "Mobile Application Penetration Test",
+        "Web Application Penetration Test"
+    );
+    $disclaimerTypeTwo = array(
+        "Infrastructure Penetration Test",
+        "O365 Penetration Test",
+        "Red Team Assessment",
+        "VPN Assessment",
+        "Vulnerability Assessment"
+    );
+
    
-    if( strpos( $typeOfService[0],  "Penetration Test" ) !== false ) {
+    if( in_array( $typeOfService[0],  $disclaimerTypeOne ) !== false ) {
 
         $projectDisclaimerPage = $phpWord->addSection(array(
             'pageSizeW' => $paper->getWidth(),
@@ -545,13 +556,16 @@
         $projectDisclaimerPage->addTitle( 'PROJECT PENTEST DISCLAIMER', 1); // TOC Bookmark
         $projectDisclaimerPage->addLine($lineStyle);
 
-        $projectDisclaimerPage->addText( htmlspecialchars("Any security testing which is conducted on the production environment needs to consider this disclaimer. RedTeam Partners security team will utilize a large proportion of manual testing part of the pen test will be conducted with the help of automated tools. The tools that are in addition to the manual verification scenarios will be included in the final report.") );
+        $projectDisclaimerPage->addText( htmlspecialchars("Any security testing which is conducted on the production environment needs to consider this disclaimer. ") );
 
-        $projectDisclaimerPage->addText( htmlspecialchars("Having a large manual coverage, will allow the security team to carefully handle the tests, only proceeding with safe-checks") );
+        $projectDisclaimerPage->addText( htmlspecialchars("Red Team Partners security team will utilize a large proportion of manual testing and part of the pen test will be conducted with the help of automated tools. The tools that are in addition to the manual verification scenarios will be included in the final report.") );
+
+        $projectDisclaimerPage->addText( htmlspecialchars("Having a large manual coverage, will allow the security team to carefully handle the tests, only proceeding with safe-checks.") );
 
         $projectDisclaimerPage->addTextBreak();
 
         $projectDisclaimerPage->addText( htmlspecialchars("During the assessment, the team will only conduct tests that will be under the Pen tester’s control.") );
+
         $projectDisclaimerPage->addText( htmlspecialchars("The penetration testing approach will be as follow: ") );
         $projectDisclaimerPage->addListItem( htmlspecialchars("Manual investigation of application code"), 0 );
         $projectDisclaimerPage->addListItem( htmlspecialchars("Passive interception of application requests"), 0 );
@@ -563,6 +577,12 @@
         $projectDisclaimerPage->addText( htmlspecialchars("During the security testing, if there will be certain functionalities that are uncertain in terms of the testing, the pen tester will notify the owner of the application and ask for approval, without conducting the specific tests."));
 
         $projectDisclaimerPage->addTextBreak();
+
+        $projectDisclaimerPage->addText( htmlspecialchars("Backup your data:"), array_merge( $boldText, $italicText ));
+        $projectDisclaimerPage->addText( htmlspecialchars("It is always a good practice for the client’s IT team to make a backup of all configurations, data, and codes before the test begins. Having a back-up will ensure that data can be restored to pre-test configurations in the event that the test may cause a system to crash or data to be lost. The Security Team recommends that the client’s internal IT or support team be readily available to resolve technical issues with the testing company during the testing phase.") );
+
+
+        $projectDisclaimerPage->addTextBreak();
         $projectDisclaimerPage->addText( htmlspecialchars("Disclaimer:"), array_merge( $boldText, $italicText ));
 
         $projectDisclaimerPage->addText( htmlspecialchars("Even though the security team will do all the security testing only with safe checks, it might be possible that a certain command that can be considered usually as a “safe-check” the back-end can interpret it in a way that can affect the whole application, through Denial of Service, data deletion or integrity loss or bad existing user experience.") );
@@ -570,6 +590,36 @@
         $projectDisclaimerPage->addText( htmlspecialchars("Another risk that should be taken into consideration is that having the testing done on production and only with certain specific tests (not going in-depth) at the pen tester’s disposal, the security team might not identify all of the vulnerabilities within the application.") );
         
     }
+
+    else if( in_array( $typeOfService[0],  $disclaimerTypeTwo ) !== false ) {
+
+        $projectDisclaimerPage = $phpWord->addSection(array(
+            'pageSizeW' => $paper->getWidth(),
+            'pageSizeH' => $paper->getHeight()
+        ));
+
+        $projectDisclaimerPage->addTitle( 'PROJECT PENTEST DISCLAIMER', 1); // TOC Bookmark
+        $projectDisclaimerPage->addLine($lineStyle);
+
+        $projectDisclaimerPage->addText( htmlspecialchars("Any security testing which is conducted on the production environment needs to consider this disclaimer.") );
+
+        $projectDisclaimerPage->addText( htmlspecialchars("Red Team Partners security team will utilize a large proportion of manual testing and part of the pen test will be conducted with the help of automated tools. The tools that are in addition to the manual verification scenarios will be included in the final report.") );
+
+        $projectDisclaimerPage->addTextBreak();
+
+        $projectDisclaimerPage->addText( htmlspecialchars("Disclaimer:"), array_merge( $boldText, $italicText ));
+
+        $projectDisclaimerPage->addText( htmlspecialchars("Even though the security team will do all the security testing only with safe checks, it might be possible that a certain command that can be considered usually as a “safe-check” the back-end can interpret it in a way that can affect the whole application/infrastructure, through Denial of Service, data deletion or integrity loss or bad existing user experience.") );
+
+        $projectDisclaimerPage->addText( htmlspecialchars("During the security testing, if there will be certain functionalities that are uncertain in terms of the testing, the pen tester will notify the owner of the application and ask for approval, without conducting the specific tests.") );
+
+        $projectDisclaimerPage->addTextBreak();
+
+        $projectDisclaimerPage->addText( htmlspecialchars("Backup your data:"), array_merge( $boldText, $italicText ));
+        $projectDisclaimerPage->addText( htmlspecialchars("It is always a good practice for the client’s IT team to make a backup of all configurations, data, and codes before the test begins. Having a back-up will ensure that data can be restored to pre-test configurations in the event that the test may cause a system to crash or data to be lost. The Security Team recommends that the client’s internal IT or support team be readily available to resolve technical issues with the testing company during the testing phase.") );
+    }
+
+
 
 
     //############################### END PROJECT DISCLAIMER PAGE ###############################   
